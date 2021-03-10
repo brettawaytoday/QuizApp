a838d5379c3e0e769af2220886b0fbe6d6382ccd
brettawaytoday <brettawaytoday@gmail.com>
2021-03-10 14:29:31 +1300

    Introduced the QuestionPresenter to add and format the title for each question. Refactored iOSViewControllerFactory to accommodate these changes.

---
+++ QuizApp/QuestionPresenter.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionPresenter.swift
+//  QuizApp
+//
+//  Created by Brett Christian on 10/03/21.
+//
+
+import Foundation
+
+struct QuestionPresenter {
+    let questions: [Question<String>]
+    let question: Question<String>
+    
+    var title: String {
+        guard let index = questions.firstIndex(of: question) else { return "" }
+        return "Question #\(index + 1)"
+    }
+}
